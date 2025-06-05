e15204dcfec59e44e6426ebe65017e757ccd5fe5
Comparitiko <gabigcy@gmail.com>
2025-06-05 10:46:54 +0200

    feat: implement user search functionality with updated input handling

--- src/components/Admin/Users/UserSearchInputs.tsx
+++ src/components/Admin/Users/UserSearchInputs.tsx
@@ -1,8 +1,10 @@
-import { Mail, UserRound } from "lucide-react";
+import { UserRound } from "lucide-react";
+import {UserSearch} from "../../../types/userSearch.ts";
+import {ChangeEvent} from "react";
 
 type Props = {
-  search: { username: string; email: string; role: string };
-  handleSearchChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+  search: UserSearch
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
 };
 
 export default function UserSearchInputs({ search, handleSearchChange }: Props) {
@@ -12,20 +14,9 @@ export default function UserSearchInputs({ search, handleSearchChange }: Props) 
         <UserRound className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-4 h-4" />
         <input
           type="text"
-          name="username"
-          placeholder="Buscar por nombre"
-          value={search.username}
-          onChange={handleSearchChange}
-          className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg text-sm shadow-sm focus:ring-2 focus:ring-green-400"
-        />
-      </div>
-      <div className="relative w-full sm:w-64">
-        <Mail className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-4 h-4" />
-        <input
-          type="text"
-          name="email"
-          placeholder="Buscar por correo"
-          value={search.email}
+          placeholder="Buscar"
+          name="search"
+          value={search.search}
           onChange={handleSearchChange}
           className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg text-sm shadow-sm focus:ring-2 focus:ring-green-400"
         />
